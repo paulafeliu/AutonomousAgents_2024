28c767834696e1d9b8d8ff0f28868122b4542e52
paulafeliu <1630423@uab.cat>
2024-03-22 15:26:50 +0100

    updated repo

--- Tic-Tac-Toe-Jason-bot/src/agt/code.py
+++
@@ -1,8 +0,0 @@
-mylist = ["","" ,"" ,"" ,"" ,"" ,"" ,"",""]
-
-if mylist[4] == "":
-    mylist[4] = "o"
-
-# for i in range(len(mylist)):
-#     if mylist[i] == "":
-#         if i 
---
+++ tictactoe/src/agt/code.py
@@ -0,0 +1,8 @@
+mylist = ["","" ,"" ,"" ,"" ,"" ,"" ,"",""]
+
+if mylist[4] == "":
+    mylist[4] = "o"
+
+# for i in range(len(mylist)):
+#     if mylist[i] == "":
+#         if i 
